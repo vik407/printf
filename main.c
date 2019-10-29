e64a86054651b5bb7381f7b4cc75a87fb1a632c0
Victor Hernandez <vik407@gmail.com>
2019-10-29 17:17:10 +0000

    Refactoring i

--- main.c
+++ main.c
@@ -9,25 +9,29 @@
  */
 int main(void)
 {
-	int len;
+	/*int len;
 	int len2;
-	/*unsigned int ui;
+	unsigned int ui;
 	void *addr;*/
-        _printf("%d\n", 5);
-	len = _printf("Let's try to printf a simple sentence.\n");
+    _printf("%i\n", 123);
+	_printf("%i\n", -123);
+	_printf("%i\n", NULL);
+	_printf("%i");
+	_printf("%i", 'a');
+	/**len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	/**ui = (unsigned int)INT_MAX + 1024;
+	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
-	_printf("Length:[%d, %i]\n", len, len);**/
+	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
-	printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);**/
 	/**_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
 	_printf("Unsigned octal:[%o]\n", ui);
 	printf("Unsigned octal:[%o]\n", ui);
 	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);**/
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("Character:[%c]\n");
@@ -35,9 +39,9 @@ int main(void)
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
 	_printf("String:[%s]\n", NULL);
-	printf("String:[%s]\n", NULL);
+	printf("String:[%s]\n", NULL);**/
 	/*_printf("Address:[%p]\n", addr);
-	  printf("Address:[%p]\n", addr);*/
+	  printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
@@ -58,6 +62,6 @@ int main(void)
 	printf("- - - - - - - - - - - - - \n");
 	len = _printf("%yd\n");
 	len = printf("%yd\n");
-	printf("- - - - - - - - - - - - - \n");
+	printf("- - - - - - - - - - - - - \n");*/
 	return (0);
 }
--- printf_i.c
+++ printf_i.c
@@ -9,30 +9,24 @@
  */
 int op_i(va_list n)
 {
-	int a, p10 = 1, len = 0;
-	unsigned int res;
+	int a, p10 = 1, len = 0, res;
 
 	a = va_arg(n, int);
-	/* Menos de 0 es negativo */
 	if (a < 0)
 	{
 		len = len + _putchar('-');
-		res = a * -1;
+		a = a * -1;
 	}
 	else
-		res = a;
-	/* Potencia 10 */
-	while (res / p10 > 9)
+		a = a;
+	while (a / p10 > 9)
 		p10 *= 10;
-	/* Mientras p10 es diferente de 0 */
 	while (p10 != 0)
 	{
-		len = len + _putchar(res / p10 + '0');
-		/* Todo tan chévere hasta error de puntero aquí me puede el conocimiento
-		* printf_i.c:30:5: error: assignment makes pointer from integer
-		* without a cast [-Werror] -> n = res % p10;*/
-		a = res % p10;
-		/* Posicionar https://www.helpingwithmath.com/by_subject/decimals/powers-of-10-5nbt2.htm */
+		len = len + 1;
+		res = a / p10;
+		a = a % p10;
+		_putchar(res + '0');
 		p10 = p10 / 10;
 	}
 	return (len);
